ba32b2df3c7cff6fccbb845b4ba4b613b4933259
karthikeyan <keyan13467@gmail.com>
2026-03-14 12:51:08 +0530

    div with pure bitwise

--- test.c
+++ test.c
@@ -1,14 +1,35 @@
-//changed
+
 #include<stdio.h>
-int power(int a,int b){
-    int res=1;
-    while(b>0){
-        if(b&1)res*=a;
-        a*=a;;
-        b>>=1;
+int add(int a,int b){
+    int carry =0;
+    while(b!=0){
+        carry=a&b;
+        a^=b;
+        b=carry<<1;
     }
-    return res;
+    return a;
 }
+
+int div(int a,int b){
+    int div=b,temp=1,q=0;
+    while(a>div){
+        div<<=1;
+        temp<<=1;
+    }
+    while(a>=b){
+        if(a>=div){
+            a=add(a,-div);
+            q=add(q,temp);
+        }
+        div>>=1;
+        temp>>=1;
+    }
+    return q;
+
+}
+
+
 int main(){
-    printf("%d",power(2,30) );
+    printf("%d",div(1236,24));
 }
+
