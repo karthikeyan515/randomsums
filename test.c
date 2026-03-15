a2162c5e6142df0288de6eb2d605b8572cb6299f
karthikeyan <keyan13467@gmail.com>
2026-03-15 21:55:42 +0530

    exponent power by binary

--- test.c
+++ test.c
@@ -1,45 +1,14 @@
+//changed
 #include<stdio.h>
-typedef struct{
-    int k;
-    int u;
-}T;
-T t[101];
- int hash(int key){
-    if(key<0) key=-key;
-    return key%101;
- }
-
-void insert (int key){
-    int idx=hash(key);
-    while(t[idx].u){
-        if(t[idx].k==key) return;
-        idx=(idx+1)%101;
+int power(int a,int b){
+    int res=1;
+    while(b>0){
+        if(b&1)res*=a;
+        a*=a;;
+        b>>=1;
     }
-    t[idx].k=key;
-    t[idx].u=1;
- }
-
- int search(int key){
-        int idx=hash(key);
-        int start=idx;
-    while(t[idx].u){
-        if(t[idx].k==key) return 1;
-        idx=(idx+1)%101;
-        if(idx==start)return 0;
-    }
- }
-
- int main(){
-    int a[5]={5,-4,12,2,1};
-    int target =8;
-    int res[2]={0};
-    for(int i=0;i<5;i++){
-        if(search(target-a[i])) {
-            res[0]=target-a[i];
-            res[1]=a[i];
-            break;
-        }
-        insert(a[i]);
-    }
-    printf("%d %d",res[0],res[1]);
- }
+    return res;
+}
+int main(){
+    printf("%d",power(2,30) );
+}
